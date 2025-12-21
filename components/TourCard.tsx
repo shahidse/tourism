22525d9c3ec91c4dc955ea4ece98032fb438dde1
shahid <shahid.hussain.se@gmail.com>
2025-12-22 04:58:12 +0500

    Remove unnecessary blank line in TourCard component for cleaner code

--- components/TourCard.tsx
+++ components/TourCard.tsx
@@ -13,7 +13,6 @@ export default function TourCard({ tour }:{tour:any}) {
         width={500} // specify appropriate width
         height={200} // specify appropriate height
       />
-
       <h3 className="text-xl font-bold text-gray-800">{tour.title}</h3>
       <p className="text-gray-600 mt-2">{tour.description}</p>
 
